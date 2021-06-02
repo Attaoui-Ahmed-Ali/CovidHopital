e920d384d4997e8c4dd24b9362a3228c10eca037
AttaouiAhmedAli <attaouiahmedali@gmail.com>
2021-06-02 23:48:39 +0200

    changes to controller

--- GestionHopital/PatientCovid/src/ma/patientcovid/ui/HopitalController.java
+++ GestionHopital/PatientCovid/src/ma/patientcovid/ui/HopitalController.java
@@ -1,5 +1,70 @@
 package ma.patientcovid.ui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class HopitalController {
 
+	@FXML
+	TableView hopitalTable;
+	
+	@FXML
+	TableColumn hopitalId;
+	
+	@FXML
+	TableColumn hopitalNom;
+	
+	@FXML
+	TableColumn hopitalVille;
+	
+	@FXML
+	Button hoptialLoad;
+	
+	@FXML
+	private void loadHopitalData() {
+		
+	}
+	
+	@FXML
+	TextField hopitalNomField;
+	
+	@FXML
+	TextField hopitalVilleField;
+	
+	@FXML
+	Button btnConfirmation;
+	
+	@FXML
+	Button btnClear;
+	
+	@FXML
+	private void addHopital() {
+		
+	}
+	
+	@FXML
+	private void clearAddHopital() {
+		
+	}
+	
+	@FXML
+	TextField hopitalIdField;
+	
+	@FXML
+	private void editHopital() {
+		
+	}
+	
+	@FXML
+	private void clearEditHopital() {
+		
+	}
+	
+	@FXML
+	private void deleteHopital() {
+		
+	}
 }
